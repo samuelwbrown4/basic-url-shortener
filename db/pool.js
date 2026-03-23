b752f5dcf6f7be837e882e4b9bc2e0c9b46e7699
Samuel Brown <samuelwbrown4@gmail.com>
2026-03-22 21:55:35 -0400

    update pool ssl

--- db/pool.js
+++ db/pool.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const { Pool } = require('pg');
 
-const pool = new Pool({connectionString: process.env.DB_CONNECTION_STRING})
+const pool = new Pool({connectionString: process.env.DB_CONNECTION_STRING , ssl: {
+    rejectUnauthorized: false
+  }})
 
 module.exports = pool;
